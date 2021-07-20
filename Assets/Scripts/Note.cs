8fea1e7c542c3558d8c5ef2d3732630726df270a
celt1125 <andysh1125@gmail.com>
2021-07-20 18:43:56 +0800

    new project.

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class Note
+{
+    string name;
+	string note_type;
+	float timing;
+	float position;
+}
---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,12 @@
+using System;
+
+[Serializable]
+public class Song
+{
+	float music_speed;
+	float music_length;
+	float drop_speed;
+	int punish;
+	int recover;
+	Note[] notes;
+}
